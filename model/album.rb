e3de0eb873fc5d3131ec0a05d96c8b20a20df79e
Joanna Gora <joanna.gora@protonmail.com>
2018-07-11 18:13:42 +0100

    created all method

--- model/album.rb
+++ model/album.rb
@@ -7,10 +7,25 @@ class Album
     @artist_id = options["artist_id"].to_i
   end
 
+  def artist
+    sql = "SELECT * FROM artists WHERE id = $1;"
+    values = [@artist_id]
+    results = SqlRunner.run(sql, values)
+    artist_hash = results.first
+    artist = Artist.new(artist_hash)
+    return artist
+  end
+
   def save
     sql = "INSERT INTO albums (title, genre, artist_id) VALUES ($1, $2, $3) RETURNING id;"
     values = [@title, @genre, @artist_id]
     albums = SqlRunner.run(sql, values)
     @id = albums[0]["id"].to_i
+  end
+
+  def self.all
+    sql = "SELECT * FROM albums"
+    albums = SqlRunner.run(sql)
+    return albums.map {|album_hash| Album.new(album_hash)}
   end
 end
